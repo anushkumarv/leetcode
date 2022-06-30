df0b89b7b18e045e1676af4ad3a4c43de2f30bba
anushkumarv <anush070@gmail.com>
2022-06-30 21:47:24 +0530

    added binary search

---
+++ binary_search/binary_search.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        
+        left, right = 0, len(nums) - 1
+        
+        while(left<=right):
+            center = (left+right) // 2
+            
+            if nums[center] == target:
+                return center
+            
+            elif nums[center] > target:
+                right = center - 1
+                
+            else:
+                left = center + 1
+                
+                
+        return -1
+
+
+sol = Solution()
+## time complexity O(log n)
+## space complexity - O(1)
+print(sol.search([-1,0,3,5,9,12], 9))
+print(sol.search([-1,0,3,5,9,12], 2))
