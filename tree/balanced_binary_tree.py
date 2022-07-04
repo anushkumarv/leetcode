0648115edeed46b4329b306f3177c619bb066305
anushkumarv <anush070@gmail.com>
2022-07-04 22:49:42 +0530

    modified balanced binary tree

--- tree/balanced_binary_tree.py
+++ tree/balanced_binary_tree.py
@@ -23,7 +23,26 @@ class MySolution:
         return valid[0]
 
 
+class SolutionClean:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(root):
+            if not root: return [True, 0]
+            
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = (left[0] and right[0] and 
+                        abs(left[1] - right[1]) <= 1)
+            return [balanced, 1 + max(left[1], right[1])]
+        return dfs(root)[0]
+
 sol = MySolution()
+## time complexity - O(n) where n is the number of nodes in the tree
+## space complexity - O(h) where h is the height of the tree
+tree = TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(7)),TreeNode(5)), TreeNode(3,right=TreeNode(6,right=TreeNode(8))))
+print(sol.isBalanced(tree))
+
+
+sol = SolutionClean()
 ## time complexity - O(n) where n is the number of nodes in the tree
 ## space complexity - O(h) where h is the height of the tree
 tree = TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(7)),TreeNode(5)), TreeNode(3,right=TreeNode(6,right=TreeNode(8))))
