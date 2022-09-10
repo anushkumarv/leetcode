e296c308d7b2e46dc1ee9da826c78f00140dae49
anushkumarv <anush070@gmail.com>
2022-09-10 11:19:56 -0600

    added clone graph

---
+++ graphs/clone_graph.py
@@ -0,0 +1,36 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if node is None:
+            return None
+        
+        visited = set()
+        nd = dict()
+        s = list()
+        fn = node.val
+        
+        s.append(node)
+        
+        while(s):
+            n = s.pop()
+            if n not in visited:
+                if n.val not in nd:
+                    nd[n.val] = Node(n.val)
+                visited.add(n)
+                for nghr in n.neighbors:
+                    if nghr.val not in nd:
+                        nd[nghr.val] = Node(nghr.val)
+                    nd[n.val].neighbors.append(nd[nghr.val]) 
+                    s.append(nghr)
+                
+        return nd[fn]
+
+
+## time complexity - O(V + E) This is depth first search
+## space complexity - O(V) using Stack and Visited for number of vertices
