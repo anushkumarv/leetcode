942e04808ca03c48f9c487d67c61a57e4cb62bb6
anushkumarv <anush070@gmail.com>
2022-09-10 12:52:39 -0600

    update house robber

--- dynamic programming/house_robber.py
+++ dynamic programming/house_robber.py
@@ -22,3 +22,22 @@ sol = Solution()
 ## space complexity - O(N)
 print(sol.rob([1,2,3,1]))
 print(sol.rob([2,1,1,2]))
+
+
+class SolutionAlternate:
+    def rob(self, nums: List[int]) -> int:
+        sol = [0] * len(nums)
+        
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[1], nums[0])
+        
+        sol[0] = nums[0]
+        sol[1] = max(nums[1], nums[0])
+        
+        for i in range(2, len(nums)):
+                sol[i] = max(sol[i-2] + nums[i], sol[i-1])
+                
+                
+        return max(sol)
