4ad912be4693d26526fd959aa06b18d628db5c6f
anushkumarv <anush070@gmail.com>
2024-06-29 11:23:44 -0600

    Added daily challenge for 28 Jun

--- daily challenge/jun24.py
+++ daily challenge/jun24.py
@@ -1,5 +1,9 @@
-from collections import heapq
+from collections import heapq, deque
 from typing import List
+
+# #####
+# https://leetcode.com/problems/maximum-total-importance-of-roads
+# #####
 
 class Solution27Jun:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
@@ -14,3 +18,45 @@ class Solution27Jun:
             res += val * freq
             val += 1
         return res
+
+
+# #####
+# https://leetcode.com/problems/all-ancestors-of-a-node-in-a-directed-acyclic-graph/
+# #####
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.ind = 0
+        self.neighbours = []
+        self.res = set()
+
+    def __repr__(self):
+        return f'node val: {self.val}'
+
+class Solution28Jun:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        nodes = [Node(i) for i in range(n)]
+        for src, tgt in edges:
+            nodes[tgt].ind += 1
+            nodes[src].neighbours.append(nodes[tgt])
+
+        q = deque()
+        for node in nodes:
+            if node.ind == 0:
+                q.append(node)
+
+        
+        while q:
+            node = q.popleft()
+            for nghr in node.neighbours:
+                nghr.ind -= 1
+                nghr.res.update(node.res)
+                nghr.res.add(node.val)
+                if nghr.ind == 0:
+                    q.append(nghr)
+
+        res = [sorted(list(nodes[i].res)) for i in range(n)]
+        
+        return res
+        
