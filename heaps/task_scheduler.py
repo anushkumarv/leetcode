ca18d7cf5e80ee8986fdc3cdbfc9b6d1d2841d59
anushkumarv <anush070@gmail.com>
2024-07-19 13:38:25 -0600

    Modified task scheduler

--- heaps/task_scheduler.py
+++ heaps/task_scheduler.py
@@ -2,6 +2,7 @@ import heapq
 from collections import deque, Counter
 from typing import List
 
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = Counter(tasks)
@@ -14,18 +15,17 @@ class Solution:
         while max_heap or q:
             time += 1
             cnt = 0
-            # print(time, max_heap, q)
-            if max_heap:
-                cnt = heapq.heappop(max_heap)
-                cnt *= -1 
-            if cnt - 1 > 0:
-                q.append((cnt-1, time + n))
+            if q and not max_heap:
+                time = q[0][1] + 1
+                heapq.heappush(max_heap, q.popleft()[0])
+            cnt = heapq.heappop(max_heap)
+            if cnt + 1 < 0:
+                q.append((cnt+1, time + n))
             if q and q[0][1] == time:
-                heapq.heappush(max_heap, q.popleft()[0] * -1)
+                heapq.heappush(max_heap, q.popleft()[0])
 
 
         return time 
-
 
 sol = Solution()
 ## time complexity - O(n)
